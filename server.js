3ec0007c83f5cd2e98e2edc9cb9105e0a6d9a121
betamee <gongxq95@gmail.com>
2017-04-30 01:00:39 +0800

    发现基本模式

--- server.js
+++ server.js
@@ -1,77 +1,23 @@
-var Server = require('bittorrent-tracker').Server
+var Tracker = require('bittorrent-tracker')
+var magnet = require('magnet-uri')
 
-// var server = new Server({
-//   udp: true, // enable udp server? [default=true]
-//   http: true, // enable http server? [default=true]
-//   ws: true, // enable websocket server? [default=true]
-//   stats: true, // enable web-based statistics? [default=true]
-//   filter: function (infoHash, params, cb) {
-//     // Blacklist/whitelist function for allowing/disallowing torrents. If this option is
-//     // omitted, all torrents are allowed. It is possible to interface with a database or
-//     // external system before deciding to allow/deny, because this function is async.
+var magnetURI = "magnet:?xt=urn:btih:670f4993c4e25d17bd9380145fcc515bfcb7fd6a&tr=http://tracker.openbittorrent.com/announce&tr=udp://tracker.openbittorrent.com:80/announce&tr=udp://tr.cili001.com:6666/announce&tr=http://tracker.publicbt.com/announce&tr=udp://open.demonii.com:1337&tr=udp://tracker.opentrackr.org:1337/announce&tr=http://tr.cili001.com:6666/announce"
 
-//     // It is possible to block by peer id (whitelisting torrent clients) or by secret
-//     // key (private trackers). Full access to the original HTTP/UDP request parameters
-//     // are available in `params`.
+var parsedTorrent = magnet(magnetURI)
 
-//     // This example only allows one torrent.
+var opts = {
+  infoHash: parsedTorrent.infoHash,
+  announce: parsedTorrent.announce,
+  peerId: new Buffer('01234567890123456789'), // hex string or Buffer
+  port: 6881 // torrent client port
+}
+console.log(parsedTorrent);
 
-//     var allowed = (infoHash === 'aaa67059ed6bd08362da625b3ae77f6f4a075aaa')
-//     if (allowed) {
-//       // If the callback is passed `null`, the torrent will be allowed.
-//       cb(null)
-//     } else {
-//       // If the callback is passed an `Error` object, the torrent will be disallowed
-//       // and the error's `message` property will be given as the reason.
-//       cb(new Error('disallowed torrent'))
-//     }
-//   }
-// })
+var client = new Tracker(opts)
 
-// // Internal http, udp, and websocket servers exposed as public properties.
-// server.http
-// server.udp
-// server.ws
+client.scrape()
 
-// server.on('error', function (err) {
-//   // fatal server error!
-//   console.log(err.message)
-// })
-
-// server.on('warning', function (err) {
-//   // client sent bad data. probably not a problem, just a buggy client.
-//   console.log(err.message)
-// })
-
-// server.on('listening', function () {
-//   // fired when all requested servers are listening
-//   console.log('listening on http port:' + server.http.address().port)
-//   console.log('listening on udp port:' + server.udp.address().port)
-// })
-
-// // start tracker server listening! Use 0 to listen on a random free port.
-// server.listen(port, hostname, onlistening)
-
-// // listen for individual tracker messages from peers:
-
-// server.on('start', function (addr) {
-//   console.log('got start message from ' + addr)
-// })
-
-// server.on('complete', function (addr) {})
-// server.on('update', function (addr) {})
-// server.on('stop', function (addr) {})
-
-// // get info hashes for all torrents in the tracker server
-// Object.keys(server.torrents)
-
-// // get the number of seeders for a particular torrent
-// server.torrents[infoHash].complete
-
-// // get the number of leechers for a particular torrent
-// server.torrents[infoHash].incomplete
-
-// // get the peers who are in a particular torrent swarm
-// server.torrents[infoHash].peers
-
-console.log(new Server());
+client.on('scrape', function (data) {
+  console.log("dd");
+  console.log(data)
+})
