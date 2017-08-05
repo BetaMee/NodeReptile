291bda52e64565a1e40c04fd3d402e7738fdefca
betamee <gongxq95@gmail.com>
2017-08-05 17:03:12 +0800

    将本仓库改为一个爬虫新项目

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
-# p2p-stream
+# Nodejs 爬虫项目
 
-使用p2p网络实现安卓端和web端的播放功能
+
+## Introduction
+
+一个使用ES7 async特性的爬虫脚本，站点来源为: [一亩三分地论坛-美国加拿大留学申请|工作就业|英语考试|学习生活信噪比最高的网站 -  Powered by Discuz!](http://www.1point3acres.com/bbs/)。最终将论坛中的留学申请>录取汇报的帖子中的数据给爬下来。
+
--- src/index.js
+++ src/index.js
@@ -1,18 +1,62 @@
-// const jquery = require('jquery');
-// const WebTorrent = require('webtorrent');
+import http from 'http';
+import Url from 'url';
+import superagent from 'superagent';
+import cheerio from 'cheerio';
 
-const client = new WebTorrent();
-console.log(client);
+/**
+ * 生成url
+ * @param {*页数} pageNum
+ */
+const generatePageUrl = (pageNum) => {
+  const urlArr = [];
+  for (let i = 0; i < pageNum; i++) {
+    urlArr.push(`http://www.1point3acres.com/bbs/forum-82-${i + 1}.html`);
+  }
+  return urlArr;
+};
 
-const magentURL = 'magnet:?xt=urn:btih:dc1138392beb1f0ecc2d2c84a40fcd9d68aba0e9&tr=http://tracker.openbittorrent.com/announce&tr=udp://tracker.openbittorrent.com:80/announce&tr=udp://tr.cili001.com:6666/announce&tr=http://tracker.publicbt.com/announce&tr=udp://open.demonii.com:1337&tr=udp://tracker.opentrackr.org:1337/announce&tr=http://tr.cili001.com:6666/announce';
+/**
+ * 解析html,获取参数链接
+ * @param {*} $
+ */
+const handleHtml = ($) => {
+  const linkArr = [];
+  $('table#threadlisttableid>tbody')
+    .each((idx, element) => {
+      const $tbody = $(element);
+      if (/^normalthread/.test($tbody.attr('id'))) {
+        linkArr.push($tbody.find('.xst').attr('href'));
+      }
+    });
+  return linkArr;
+};
+
+/**
+ * 处理响应
+ * @param {*响应} response
+ */
+const handleResponse = (response) => {
+  const results = [];
+  for (let i = 0; i < response.length; i++) {
+    const $ = cheerio.load(response[0].text); // 将每一个页面用cheerio处理
+    const linkArr = handleHtml($); // 传给handleHtml，返回一个链接数组
+    results.push(...linkArr);
+  }
+  console.log(results.length);
+  return results;
+};
 
 
-client.add(magentURL,function(torrent) {
-  console.log('CLient is Downloading');
-  console.log(torrent);
+/**
+ * 异步获取汇报页的链接
+ */
+const asyncGetForumPage = async () => {
+  const pageUrlArr = generatePageUrl(1000);
+  const results = [];
+  for (let i = 0; i < pageUrlArr.length; i++) {
+    results.push(superagent.get(pageUrlArr[i]));
+  }
+  handleResponse(await Promise.all(results));
+};
 
-});
-
-client.on('torrent',function(torrent) {
-  console.log(torrent);
-});
+asyncGetForumPage();
